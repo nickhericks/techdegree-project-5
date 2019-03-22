ceab0ca03e961b79a43294353011e9c69b5184b3
Nick-Hericks <nickhericks@gmail.com>
2019-03-22 11:11:56 +0200

    Fix closeModal

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ let employeeList;
 gallery.addEventListener('click', e => openModal(e));
 
 
-
 // Request data for 12 random users
 fetch('https://randomuser.me/api/?results=12')
 	.then( response => response.json() )
@@ -35,14 +34,14 @@ function displayEmployees(employees) {
 	gallery.innerHTML = html;
 }
 
-
+// Close modal
 function closeModal() {
 	const modalContainer = document.querySelector('.modal-container');
 	modalContainer.remove();
 }
 
 
-
+// Open modal
 function openModal(event) {
 	// Find ancestor element with the class of 'card'
 	let card = event.target.closest('.card');
@@ -61,27 +60,42 @@ function openModal(event) {
 		// console.log(employee);
 
 		const modalHTML = `
-		  <div class="modal-container">
+			<div class="modal-container">
 				<div class="modal">
 					<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
 					<div class="modal-info-container">
 						<img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-						<h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+						<h3 id="name" class="modal-name cap">
+							${employee.name.first} ${employee.name.last}
+						</h3>
 						<p class="modal-text">${employee.email}</p>
 						<p class="modal-text cap">${employee.location.city}</p>
 						<hr>
 						<p class="modal-text">${employee.cell}</p>
-						<p class="modal-text cap">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+						<p class="modal-text cap">
+							${employee.location.street}, 
+							${employee.location.city}, 
+							${employee.location.state} ${employee.location.postcode}
+						</p>
 						<p class="modal-text">Birthday: ${birthday}</p>
 					</div>
 				</div>
 			</div>
 		`;
-		gallery.insertAdjacentHTML('afterend', modalHTML);
-		// console.log(modalHTML);
 
+		gallery.insertAdjacentHTML('afterend', modalHTML);
 
 		let closeButton = document.querySelector('.modal-close-btn');
 		closeButton.addEventListener('click', e => closeModal(e));
+
+
+		document.querySelector('.modal-container').addEventListener('click', e => {
+			if (!e.target.closest('.modal')) {
+				closeModal();
+			}
+		});
+
+
 	}
 }
+
